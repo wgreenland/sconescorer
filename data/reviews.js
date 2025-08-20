57ddaddfa357633e7147c36b3bba76a340d5b11e
William Greenland <wgreenland2000@gmail.com>
2025-08-20 22:46:05 +1200

    Using real reviews (first three)

--- data/reviews.js
+++ data/reviews.js
@@ -1,42 +1,60 @@
+// /data/reviews.js
 window.REVIEWS = [
   {
-    id: "2025-08-18-floriditas",
-    date: "2025-08-18",                 // ISO (YYYY-MM-DD)
-    cafe: "Floriditas Cafe",
-    location: "Wellington, NZ",
+    id: "2025-08-20-meshino",
+    date: "2025-08-20",
+    cafe: "Meshino Cafe",
+    location: "75 Rutland Street, Christchurch, New Zealand 8014",
     links: {
-      website: "https://floriditas.co.nz",
-      instagram: "",
+      instagram: "https://www.instagram.com/meshino_cafe/",
+      website: "",
       facebook: ""
     },
-    review: "Perfect golden crust with super fluffy interior and aged cheddar throughout. Likely a touch of sour cream. Premium but worth it.",
-    score: 9.8
+    review: `Kia ora all!!
+
+Scone-scorer here! I’ll be posting short written reviews for all the sconnoisseurs out there as I tour Aotearoa searching for the perfect savoury snack. A bit about me to begin with? Nah, this page isn’t about me. It’s about you. It’s about getting you to that ultimate scone, to the comfort of that cheesy, moist dough in a foreign land. 
+
+To get you started on that journey, I’ve unearthed a little gem hidden in the expansive backstreets of Christchurch’s inner north. I stumbled into Meshino Cafe half delirious after a 2 hour trudge back from an unsuccessful mish to AA in Shirley. The early signs were good. A weatherboard cafe perched elegantly on a quiet street corner, it was clear that the patrons weren’t just idle passersby. They had come with intention. As I had.
+
+On account of being underdressed for the chill of a late May Cantabrian morning, I made the unusual decision to take this scone home with me. And bloody hell, she can stay! This hunky piece of dough was exactly what I dream about when I think of cheese scones. A simple smattering of chives combined beautifully with an even distribution of cheese and a thin, flaky crust. Hecker, I’m getting hungry just writing about it! 
+
+After three scone-less days in Aotearoa, this scone crushed the withdrawal shakes. A strong 9/10 to kick off the series. On reflection, I wonder whether Meshino’s, which sits directly opposite Rugby Park, has any role to play in the Crusaders dominance of Southern Hemisphere Rugby (8 in 9 🏆 🏆 🏆 thanks for coming). If so, then I bet Ethan Blackadder and his boys are ordering the cheese scones each morning. 
+
+Chur bro!`,
+    score: 9.1
   },
   {
-    id: "2025-08-15-astro",
-    date: "2025-08-15",
-    cafe: "Astro Cafe",
-    location: "Auckland, NZ",
+    id: "2025-08-19-queenstowncafe",
+    date: "2025-08-19",
+    cafe: "Queenstown Cafe",
+    location: "87 Gorge Road, Queenstown, New Zealand 9300",
     links: {
-      website: "https://astrocafe.co.nz",
-      instagram: "",
+      instagram: "https://www.instagram.com/queenstowncafe/",
+      website: "",
       facebook: ""
     },
-    review: "Great texture; crispy outside, fluffy inside. Cheese blend tastes like cheddar + gruyère. Could use a pinch more salt.",
-    score: 9.5
+    review: `Queenstown meet scone scorer, scone scorer meet Queenstown. After a tedious 8.5-hour bus ride from Christchurch (thank goodness for the jaw-dropping scenery) I was keen to sink my teeth into the Wakatipu Scone Scene. To identify the best options, I did what any good military general would do and scaled the nearest peak to survey the surrounds.
+
+With my trusty team of goat scouts in tow, I made quick work of Ben Lomond (1748m) and neighbouring Bowen Peak (1630m). Visibility was picture perfect, allowing for detailed note-taking of potential scone opportunities. With such a long list ahead of me, the only logical option was to commence with the town's namesake cafe. Situated somewhat misleadingly on the outskirts of town, the rustic-looking Queenstown Cafe appeared to offer plenty of promise. I don't know if it was the timber or just a hypersensitive nose, but this venue was heavy on smells. Good ones mostly. Rich coffee odours wafted across my windowsill table, intermingling with various scents from a smorgasbord of pie options. As if these olfactory factors weren't sufficient, the odds of a strong scone were heightened by my ravenous hunger (28km and 2000m elevation does that to a man).
+
+Tragically, however, this scone was unable to rise to the occasion. The texture was doughy and flavours basic. Some chives may have helped, but I left wondering whether a Monday afternoon was the optimal time for a scone. Do bakers bake on a Monday morning? Maybe not if they've just worked both days on the weekend. Or perhaps I'm selling them short. Either way, a harsh - but I think fair - 3.2/10. Will likely get a second chance on account of being close to my bus stop. Would be thrilled to be proven wrong.`,
+    score: 3.2
   },
   {
-    id: "2025-08-15-astro",
-    date: "2025-08-15",
-    cafe: "Penis Cafe",
-    location: "Arrowtown, NZ",
+    id: "2025-08-18-thebakery",
+    date: "2025-08-18",
+    cafe: "The Bakery",
+    location: "37 Shotover Street, Queenstown, New Zealand 9300",
     links: {
-      website: "https://astrocafe.co.nz",
-      instagram: "",
+      instagram: "https://www.instagram.com/queenstownbakery/",
+      website: "",
       facebook: ""
     },
-    review: "Great texture; crispy outside, fluffy inside. Cheese blend tastes like cheddar + gruyère. Could use a pinch more salt.",
-    score: 6.9
+    review: `Dejected from my Queenstown Cafe experience, I was idly wandering in the direction of home for the evening (check out the lovely folks at Absoloot Hostel if you’re in town and in need of a bed for the night) when a glimmer of hope appeared in the corner of my downcast gaze. The Bakery. Any institution that bestows on itself a definite article immediately excites my attention. Given my current preoccupation with the humble scone (I fall asleep dreaming of these things) the pull of attraction to this simple looking facade was magnetic. Alas, I had arrived too late in the afternoon to contemplate the possibility of a double scone day. This was to be a next day mission.
+
+Waking the next morning to a fog-shrouded Queenstown, I was equal parts thankful for yesterday’s successful aerial reconnaissance mission, and thrilled about the possibility of a redemption scone at THE Bakery. Little birdies had been singing the praises of their pies, so I figured they also excelled in the art of other baked goods, particularly those of the savoury persuasion. For this mission I brought along Tom Holland’s Dominion: The Making of the Western Mind. A rather meaty tome, I was hoping to gain valuable new insight into the foundational precepts of Western thought. Alas my monkey brain and childish aversion to all things spiritual put me off almost from the first page. 
+
+When I reflect upon the merits of the scone, cheerily delivered to my street-facing table warmed and buttered, I wonder if this frame of mind negatively impacted my review. Probably. Nonetheless, I don’t pretend to be impartial. Nor should anybody else, and if they say they are then they’re lying. Anyway, dodging that philosophical question I have to say that this scone suffered from the same defects as its local compatriot. The dough was slightly less chewy, but again I could not help but feel they needed to invest in better dairy. This was a 4.6/10 in my books. On a side note, and at risk of stepping on the toes of @piemantravels, their pies are delicious. Would highly recommend any visitors come here rather than the Ferg behemoth directly across Shotover Street (though I have to admit that Fergbaker might do the best pie in the country…)`,
+    score: 4.6
   }
-  // Add more reviews here as you go
 ];
